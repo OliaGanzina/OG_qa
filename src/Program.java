bc35b63381673c266a423f57cd58d51b9fb675b7
OliaGanzina <ganzina.03@gmail.com>
2021-10-20 14:14:02 +0300

    Task 1

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,12 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+
+        int n = 18;
+        int c = n % 2;
+            if (c == 0) {
+            System.out.println("1. 18 - Even number");}
+            else {
+            System.out.println("1. 18 - Odd number");}
+
     }
 }
