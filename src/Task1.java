366a47871a22044e01f668c08c8d79133bbd5525
OliaGanzina <ganzina.03@gmail.com>
2021-10-20 14:28:07 +0300

    Added Task 1, 2

--- src/Task1.java
+++ src/Task1.java
@@ -1,12 +1,12 @@
-public class Program {
+public class Task1 {
     public static void main(String[] args) {
 
         int n = 18;
         int c = n % 2;
             if (c == 0) {
-            System.out.println("1. 18 - Even number");}
+            System.out.println("18 - Even number");}
             else {
-            System.out.println("1. 18 - Odd number");}
+            System.out.println("18 - Odd number");}
 
     }
 }
---
+++ src/Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        double m = 9.8;
+        double n = 12.3;
+        double x = 10.0 - 9.8;
+        double y = 12.3 - 10.0;
+            if (x < y) {
+                System.out.println(m);
+                System.out.println("Closest to 10");}
+            else {
+                System.out.println(n);
+                System.out.println("Closest to 10");
+        }
+    }
+}
